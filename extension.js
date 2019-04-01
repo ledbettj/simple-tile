156302f2b383abd4d3e37082a8b6fb6af7e3d6df
John Ledbetter <john@callrail.com>
2019-04-01 15:16:11 -0400

    3.32 update + es6 classes

--- extension.js
+++ extension.js
@@ -36,210 +36,213 @@ function disable() {
 }
 
 
-function SimpleTile(settings) {
-  this.settings = settings;
-  this.keybindings = {};
-  this.signals = [];
-}
+var SimpleTile = class SimpleTile {
 
-
-SimpleTile.prototype.startup = function() {
-  this.getScreenGeometry();
-  this.registerKeybindings();
-  this.connectSignals();
-};
-
-SimpleTile.prototype.shutdown = function() {
-  this.unregisterKeybindings();
-  this.disconnectSignals();
-};
-
-SimpleTile.prototype.getScreenGeometry = function() {
-  let displayCount = Main.layoutManager.monitors.length,
-      primary      = Main.layoutManager.primaryIndex;
-
-  this.screens = [];
-
-  for(let i = 0; i < displayCount; ++i) {
-    let geo = Main.layoutManager.getWorkAreaForMonitor(i);
-    this.screens[i] = {
-      x: geo.x,
-      y: geo.y,
-      w: geo.width,
-      h: geo.height
-    };
+  constructor(settings) {
+    this.settings = settings;
+    this.keybindings = {};
+    this.signals = [];
   }
 
-  this.primaryScreen = this.screens[primary];
-};
+  startup() {
+    this.getScreenGeometry();
+    this.registerKeybindings();
+    this.connectSignals();
+  }
 
-SimpleTile.prototype.getWindowMonitor = function(window) {
-  return this.screens[window.get_monitor()];
-};
+  shutdown() {
+    this.unregisterKeybindings();
+    this.disconnectSignals();
+  }
 
-SimpleTile.prototype.nextMonitor = function(index) {
-  return this.screens[(index + 1) % this.screens.length];
-};
+  getScreenGeometry() {
+    let displayCount = Main.layoutManager.monitors.length,
+        primary      = Main.layoutManager.primaryIndex;
 
-SimpleTile.prototype.registerKeybindings = function() {
-  KEYBINDINGS.forEach(function(binding) {
-    var handler = binding.split('-').join('_');
-    Main.wm.addKeybinding(
-      binding,
-      this.settings,
-      Meta.KeyBindingFlags.NONE,
-      Shell.ActionMode.NORMAL,
-      Lang.bind(this, this[handler])
+    this.screens = [];
+
+    for(let i = 0; i < displayCount; ++i) {
+      let geo = Main.layoutManager.getWorkAreaForMonitor(i);
+      this.screens[i] = {
+        x: geo.x,
+        y: geo.y,
+        w: geo.width,
+        h: geo.height
+      };
+    }
+
+    this.primaryScreen = this.screens[primary];
+  }
+
+  getWindowMonitor(window) {
+    return this.screens[window.get_monitor()];
+  }
+
+  nextMonitor(index) {
+    return this.screens[(index + 1) % this.screens.length];
+  }
+
+  registerKeybindings() {
+    KEYBINDINGS.forEach(function(binding) {
+      var handler = binding.split('-').join('_');
+      Main.wm.addKeybinding(
+        binding,
+        this.settings,
+        Meta.KeyBindingFlags.NONE,
+        Shell.ActionMode.NORMAL,
+        this[handler].bind(this)
+      );
+    }, this);
+  }
+
+  unregisterKeybindings() {
+    KEYBINDINGS.forEach(function(binding) {
+      Main.wm.removeKeybinding(binding);
+    });
+  }
+
+  tile_left_top() {
+    let window = global.display.focus_window;
+    let screen = this.getWindowMonitor(window);
+
+    window.move_resize_frame(
+      false,
+      screen.x,
+      screen.y,
+      screen.w / 2,
+      screen.h / 2
     );
-  }, this);
-};
+  }
 
-SimpleTile.prototype.unregisterKeybindings = function() {
-  KEYBINDINGS.forEach(function(binding) {
-    Main.wm.removeKeybinding(binding);
-  });
-};
+  tile_right_top() {
+    let window = global.display.focus_window;
+    let screen = this.getWindowMonitor(window);
 
-SimpleTile.prototype.tile_left_top = function() {
-  let window = global.display.focus_window;
-  let screen = this.getWindowMonitor(window);
+    window.move_resize_frame(
+      false,
+      screen.x + screen.w / 2,
+      screen.y,
+      screen.w / 2, screen.h / 2
+    );
+  }
 
-  window.move_resize_frame(
-    false,
-    screen.x,
-    screen.y,
-    screen.w / 2,
-    screen.h / 2
-  );
-};
+  tile_left_bottom() {
+    let window = global.display.focus_window;
+    let screen = this.getWindowMonitor(window);
 
-SimpleTile.prototype.tile_right_top = function() {
-  let window = global.display.focus_window;
-  let screen = this.getWindowMonitor(window);
+    window.move_resize_frame(
+      false,
+      screen.x,
+      screen.y + screen.h / 2,
+      screen.w / 2,
+      screen.h / 2
+    );
+  }
 
-  window.move_resize_frame(
-    false,
-    screen.x + screen.w / 2,
-    screen.y,
-    screen.w / 2, screen.h / 2
-  );
-};
+  tile_right_bottom() {
+    let window = global.display.focus_window;
+    let screen = this.getWindowMonitor(window);
 
-SimpleTile.prototype.tile_left_bottom = function() {
-  let window = global.display.focus_window;
-  let screen = this.getWindowMonitor(window);
+    window.move_resize_frame(
+      false,
+      screen.x + screen.w / 2,
+      screen.y + screen.h / 2,
+      screen.w / 2,
+      screen.h / 2
+    );
+  }
 
-  window.move_resize_frame(
-    false,
-    screen.x,
-    screen.y + screen.h / 2,
-    screen.w / 2,
-    screen.h / 2
-  );
-};
+  tile_left_full() {
+    let window = global.display.focus_window;
+    let screen = this.getWindowMonitor(window);
 
-SimpleTile.prototype.tile_right_bottom = function() {
-  let window = global.display.focus_window;
-  let screen = this.getWindowMonitor(window);
+    window.move_resize_frame(
+      false,
+      screen.x,
+      screen.y,
+      screen.w / 2,
+      screen.h
+    );
+  }
 
-  window.move_resize_frame(
-    false,
-    screen.x + screen.w / 2,
-    screen.y + screen.h / 2,
-    screen.w / 2,
-    screen.h / 2
-  );
-};
+  tile_right_full() {
+    let window = global.display.focus_window;
+    let screen = this.getWindowMonitor(window);
 
-SimpleTile.prototype.tile_left_full = function() {
-  let window = global.display.focus_window;
-  let screen = this.getWindowMonitor(window);
+    window.move_resize_frame(
+      false,
+      screen.x + screen.w / 2,
+      screen.y,
+      screen.w / 2,
+      screen.h
+    );
+  }
 
-  window.move_resize_frame(
-    false,
-    screen.x,
-    screen.y,
-    screen.w / 2,
-    screen.h
-  );
-};
+  tile_top_full() {
+    let window = global.display.focus_window;
+    let screen = this.getWindowMonitor(window);
 
-SimpleTile.prototype.tile_right_full = function() {
-  let window = global.display.focus_window;
-  let screen = this.getWindowMonitor(window);
+    window.move_resize_frame(false, screen.x, screen.y, screen.w, screen.h / 2);
+  }
 
-  window.move_resize_frame(
-    false,
-    screen.x + screen.w / 2,
-    screen.y,
-    screen.w / 2,
-    screen.h
-  );
-};
+  tile_bottom_full() {
+    let window = global.display.focus_window;
+    let screen = this.getWindowMonitor(window);
 
-SimpleTile.prototype.tile_top_full = function() {
-  let window = global.display.focus_window;
-  let screen = this.getWindowMonitor(window);
+    window.move_resize_frame(
+      false,
+      screen.x + screen.w / 2,
+      screen.y + screen.h / 2,
+      screen.w,
+      screen.h / 2
+    );
+  }
 
-  window.move_resize_frame(false, screen.x, screen.y, screen.w, screen.h / 2);
-};
+  tile_switch_monitor() {
+    let window       = global.display.focus_window;
+    let monitorIndex = window.get_monitor();
+    let rect         = window.get_frame_rect();
+    let oldScreen    = this.screens[monitorIndex];
+    let newScreen    = this.nextMonitor(monitorIndex);
 
-SimpleTile.prototype.tile_bottom_full = function() {
-  let window = global.display.focus_window;
-  let screen = this.getWindowMonitor(window);
+    let xscale = newScreen.w  / oldScreen.w;
+    let yscale = newScreen.h / oldScreen.h;
+    let xoff   = rect.x - oldScreen.x;
+    let yoff   = rect.y - oldScreen.y;
 
-  window.move_resize_frame(
-    false,
-    screen.x + screen.w / 2,
-    screen.y + screen.h / 2,
-    screen.w,
-    screen.h / 2
-  );
-};
+    window.move_resize_frame(
+      false,
+      Math.floor(newScreen.x + xoff * xscale),
+      Math.floor(newScreen.y + yoff * yscale),
+      Math.floor(rect.width  * xscale),
+      Math.floor(rect.height * yscale)
+    );
+  }
 
-SimpleTile.prototype.tile_switch_monitor = function() {
-  let window       = global.display.focus_window;
-  let monitorIndex = window.get_monitor();
-  let rect         = window.get_frame_rect();
-  let oldScreen    = this.screens[monitorIndex];
-  let newScreen    = this.nextMonitor(monitorIndex);
+  tile_full() {
+    let window = global.display.focus_window;
+    let screen = this.getWindowMonitor(window);
 
-  let xscale = newScreen.w  / oldScreen.w;
-  let yscale = newScreen.h / oldScreen.h;
-  let xoff   = rect.x - oldScreen.x;
-  let yoff   = rect.y - oldScreen.y;
+    window.move_resize_frame(
+      false,
+      screen.x,
+      screen.y,
+      screen.w,
+      screen.h
+    );
+  }
 
-  window.move_resize_frame(
-    false,
-    Math.floor(newScreen.x + xoff * xscale),
-    Math.floor(newScreen.y + yoff * yscale),
-    Math.floor(rect.width  * xscale),
-    Math.floor(rect.height * yscale)
-  );
-};
+  connectSignals() {
+    this.signals.push(
+      Main.layoutManager.connect('monitors-changed', this.getScreenGeometry.bind(this))
+    );
+  }
 
-SimpleTile.prototype.tile_full = function() {
-  let window = global.display.focus_window;
-  let screen = this.getWindowMonitor(window);
+  disconnectSignals() {
+    this.signals.forEach(function(signal) {
+      Main.layoutManager.disconnect(signal);
+    });
+    this.signals = [];
+  }
 
-  window.move_resize_frame(
-    false,
-    screen.x,
-    screen.y,
-    screen.w,
-    screen.h
-  );
-};
-
-SimpleTile.prototype.connectSignals = function() {
-  this.signals.push(
-    Main.layoutManager.connect('monitors-changed', Lang.bind(this, this.getScreenGeometry))
-  );
-};
-
-SimpleTile.prototype.disconnectSignals = function() {
-  this.signals.forEach(function(signal) {
-    Main.layoutManager.disconnect(signal);
-  });
-  this.signals = [];
 };
